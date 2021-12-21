61e5fa9d0d975fd1350914daaf72d3b7d31ad5a8
nuruzzamanhimel0 <nuruzzaman.himel147@gmail.com>
2021-12-21 21:58:47 +0600

    'done'

---
+++ src/store/store.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+  const store = new Vuex.Store({
+    state: {
+        msg:"My name is himel"
+    },
+    mutations: {
+
+    },
+    actions:{
+
+    },
+    getters:{
+
+    }
+});
+
+export default store;
