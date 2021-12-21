d49325d9a71affb57ed202e5351a78a918c71db0
nuruzzamanhimel0 <nuruzzaman.himel147@gmail.com>
2021-12-22 00:00:09 +0600

    'done'

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,34 +1,51 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <button @click.prevent="updatMessage" >Update Message</button>
+    <br>
+
+    <ul>
+      <li v-for="(value,key) in getUsersAge" :key="key">
+        {{value}}
+      </li>
+    </ul>
 
   </div>
 </template>
 
 <script>
+import { mapState , mapGetters } from 'vuex'
+
 export default {
   data(){
     return {
-      msg: this.$store.state.msg,
+      // msg: this.$store.state.msg,
+    }
+  },
+  mounted(){
+
+  },
+  computed:{
+    //sprate opperator
+    ...mapState(['msg']),
+    ...mapGetters(['getUsersAge']),
+
+  },
+  methods:{
+    updatMessage(){
+      this.$store.dispatch("updateMessageActions");
     }
   }
 }
+
+// computed:{
+//   msg(){
+//     return this.$store.state.msg
+//   },
+//   getUser(){
+//     return this.$store.getters.getUsersAge;
+//   }
+// },
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+
--- src/store/store.js
+++ src/store/store.js
@@ -5,17 +5,35 @@ Vue.use(Vuex)
 
   const store = new Vuex.Store({
     state: {
-        msg:"My name is himel"
+        msg:"My name is himel",
+        users:[
+            {
+                'id':1,"name":"himel","age":40
+            },{
+                "id":2,"name":"toam;",'age':20
+            }
+        ]
     },
-    mutations: {
+      getters:{
+          updateMessageGetters(state){
+              return state.msg;
+          },
+          getUsersAge(state){
+              return state.users.filter(user => user.age <40);
+          }
 
+      },
+    mutations: {
+        updateMessageMutation(state){
+            state.msg =" Update Message"
+        }
     },
     actions:{
-
+        updateMessageActions(context){
+            context.commit('updateMessageMutation');
+        }
     },
-    getters:{
 
-    }
 });
 
 export default store;
